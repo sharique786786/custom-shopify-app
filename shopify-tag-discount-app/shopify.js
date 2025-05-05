af5bad58227df2fed1d3d457f2ebd9fe49c5a05d
sharique786786 <sharique.khan@mail.valuecoders.com>
2025-05-05 13:17:43 +0530

    Update shopify.js

--- shopify-tag-discount-app/shopify.js
+++ shopify-tag-discount-app/shopify.js
@@ -3,8 +3,8 @@ import { shopifyApi, MemorySessionStorage } from '@shopify/shopify-api';
 const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET,
-  scopes: process.env.SCOPES.split(','),
-  hostName: process.env.HOST.replace(/^https:\/\//, ''),
+  scopes: process.env.SHOPIFY_SCOPES.split(','),
+  hostName: process.env.SHOPIFY_APP_HOST.replace(/^https:\/\//, ''),
   isEmbeddedApp: true,
   apiVersion: '2024-01',
   sessionStorage: new MemorySessionStorage(), // ✅ Correct usage
