2d71209ecba474a6b2e13f9164bd5ad2268becef
sharique786786 <sharique.khan@mail.valuecoders.com>
2025-06-19 17:26:06 +0530

    Update build.js

--- shopify-tag-discount-app/extensions/tag-discount-function/build.js
+++ shopify-tag-discount-app/extensions/tag-discount-function/build.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
 
 const source = path.resolve('src/run.js');
 const destinationDir = path.resolve('dist');
